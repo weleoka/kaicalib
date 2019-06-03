8fb31b0383b72335533e33fdf50bae9b1f3ab7fd
Kai Weeks <kai.weeks@student-ltu.se>
2019-06-03 09:04:22 -0700

    Implements SpringBootServletInitializer

--- src/main/java/se/ltu/kaicalib/KaicalibApplication.java
+++ src/main/java/se/ltu/kaicalib/KaicalibApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import se.ltu.kaicalib.core.config.KaicalibYamlConfig;
 
 import javax.annotation.PostConstruct;
@@ -14,21 +16,34 @@ import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class KaicalibApplication {
+public class KaicalibApplication extends SpringBootServletInitializer {
 
     private static Logger logger = LoggerFactory.getLogger(KaicalibApplication.class);
 
     @Autowired
     private KaicalibYamlConfig yamlConfig;
 
+
+    /**
+     * Used when run as JAR
+     */
     public static void main(String[] args) {
         logger.debug("STARTING APPLICATION!");
         SpringApplication.run(KaicalibApplication.class, args); // "--debug");
     }
 
+
+    /**
+     * Used when run as WAR
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(KaicalibApplication.class);
+    }
+
+
     @PostConstruct
     private void init(){
         logger.info(yamlConfig.toString());
     }
-
 }
